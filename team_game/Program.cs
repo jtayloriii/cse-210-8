7d00b6af10efacbcec59cc8f7223610d42cb45c4
jtayloriii <98050737+jtayloriii@users.noreply.github.com>
2022-10-04 14:39:30 -0600

    basic main

--- team_game/Program.cs
+++ team_game/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        player player_points = new points();
+        player card_val = new card
+
+        player_points = 300;
+
+        while (player_points != 0)
+        {
+            Console.WriteLine($"The card is: {card_val}");
+            Console.WriteLine("Hello, World!");
+
+
+        }
+
+        DisplayBoard(board);
+        Console.WriteLine("Good game. Thanks for playing!");
+    }
+}
