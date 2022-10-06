fd76df338ddf4e4ca1619a7aade1eadcadb71643
camdenmillsboi <camoflauge@byui.edu>
2022-10-06 14:38:26 -0600

    Update card.cs

--- team_game/card.cs
+++ team_game/card.cs
@@ -1,6 +1,7 @@
 class card
 {
     List<string> cards = new List<string>();
+    
     public card() {
         for (int i = 1; i <= 13; i += 1)
         {
@@ -8,18 +9,27 @@ class card
         }
     var random = new Random();
         int index = random.Next(cards.Count);
+        string newCard = (cards[index]);
         Console.WriteLine(cards[index]);
     }
+    public string Description()   {
+        string nameOfCard = newCard;
+        if nameOfCard == "1";
+    return  $"{nameOfCard}";
+    }
+
+
+
 }
 
 class player
 {
 
-    public player()  {
-        
+    public player ()  {
         int player = 300;
         int incorrect = 50;
         int correct = 100;
+
 
 
 
