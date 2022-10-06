09aa4d487977cda451a08c3112fa9f0b1668e1bb
jtayloriii <98050737+jtayloriii@users.noreply.github.com>
2022-10-06 14:37:26 -0600

    pull

--- team_game/Program.cs
+++ team_game/Program.cs
@@ -1,20 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
         player player_points = new points();
-        player card_val = new card
-
-        player_points = 300;
+        card card_val = new card();
 
         while (player_points != 0)
         {
             Console.WriteLine($"The card is: {card_val}");
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Higher/Lower? [h/l]: ");
             string input_ans = Console.ReadLine();
 
+            if (input_ans == "h")
+            {
+                card new_card = new card();
+
+                if (new_card.number < card_val.number)
+                {
+                    player_points -= 75;
+                }
+                if (new_card.number > card_val.number)
+                {
+                    player_points += 100;
+                }
+
+            }
+
+            else if (input_ans == "l")
+            {
+                card new_card = new card();
+
+                if (new_card.number < card_val.number)
+                {
+                    player_points -= 75;
+                }
+                if (new_card.number > card_val.number)
+                {
+                    player_points += 100;
+                }
+            }
+            else 
+            {
+                Console.WriteLine("I hate you, go away.");
+            }
+        }
+        
+        Console.WriteLine("Good game!");
+        Console.WriteLine("Would you like to play again? [y/n] ");
+        string play_gen = Console.ReadLine();
+
+        if (play_gen == "y")
+        {
+            Main();
+        }
+        else 
+        {
+            return 0;
         }
 
         DisplayBoard(board);
