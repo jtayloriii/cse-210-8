2d987b2ffa102bbf409555a35c7fb025ba107ad8
jtayloriii <98050737+jtayloriii@users.noreply.github.com>
2022-12-04 18:09:57 -0700

    jumper program

---
+++ jumper/classes.cs
@@ -0,0 +1,119 @@
+class Jumper 
+{
+
+    public string first =  "    ___";
+    public string second = "  /_____\\";
+    public string third =  "  \\     / ";
+    public string fourth = "   \\   /  ";
+
+    public void oneWrong() 
+    {
+        Console.WriteLine($"{second}");
+        Console.WriteLine($"{third}");   
+        Console.WriteLine($"{fourth}");     
+    }
+
+    public void twoWrong() 
+    {
+        Console.WriteLine($"{third}");   
+        Console.WriteLine($"{fourth}");           
+    }
+
+    public void threeWrong() 
+    { 
+        Console.WriteLine($"{fourth}");           
+    }
+
+    public void fourWrong() 
+    {
+        string first =  "     X";
+        string second = "    /|\\";
+        string third =  "    / \\";   
+        string fourth = "^^^^^^^^^^^^";
+        Console.WriteLine($"{first}");
+        Console.WriteLine($"{second}");
+        Console.WriteLine($"{third}");   
+        Console.WriteLine($"{fourth}");  
+    }
+
+    public void displayPerson() 
+    {
+        string first =  "     O";
+        string second = "    /|\\";
+        string third =  "    / \\";
+        Console.WriteLine($"{first}");
+        Console.WriteLine($"{second}");
+        Console.WriteLine($"{third}");
+    }
+}
+
+class Word 
+{
+    public string getWord() 
+    {
+        List<string> words = new List<string> {"heart", "vigor", "power", "might", "love", "force"};
+        Random rnd = new Random();
+        int rndIndex = rnd.Next(words.Count);
+
+        return words[rndIndex];
+    }
+
+    public int getIndex(List<string> inList, string guess)
+    {
+        int index = inList.FindIndex(a => a.Contains(guess));
+        return index;
+    }
+
+
+}
+
+class Answer
+{
+    public List<string> parseWord(string word)
+    {
+        List<string> parsedWord =  new List<string>();
+        foreach (char c in word) 
+        {
+            string s = c.ToString();
+            parsedWord.Add(s);
+        }
+        return parsedWord;
+    }
+
+    public string answerLine(string word) // (add: "char guess")
+    {
+        string answer = "";
+        for (int i = 0; i < word.Length; i++)
+        {
+            answer = answer + "_";
+        }
+        return answer;
+    }
+
+    public string newLine(string line, string guess, int index)
+    {
+        char[] changeArray = line.ToCharArray();
+        changeArray[index] = char.Parse(guess); // index starts at 0!
+        string newLine = new string (changeArray);
+
+        return newLine;
+    }
+}
+
+class Guess 
+{
+    public int guessNum = 0;
+    public int numRight = 0;
+    public int numWrong = 0;
+    public string getGuess()
+    {
+        Console.WriteLine("Please make your guess: ");
+
+        string? guess1 = Console.ReadLine();
+        if (guess1 is null) 
+        {
+            return "";
+        }
+        return guess1;
+    }
+}
---
+++ jumper/program.cs
@@ -0,0 +1,96 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Jumper jumper = new Jumper();
+        Word newWord = new Word();
+        Answer newAnswer = new Answer();
+        Guess newGuess = new Guess();
+
+        Console.WriteLine($"{jumper.first}");
+        Console.WriteLine($"{jumper.second}");
+        Console.WriteLine($"{jumper.third}");   
+        Console.WriteLine($"{jumper.fourth}"); 
+        jumper.displayPerson(); 
+        Console.WriteLine();
+
+        string word = newWord.getWord();
+        Console.WriteLine(word);
+
+        List<string> parsedWord =  new List<string>();
+        parsedWord = newAnswer.parseWord(word);
+        string displayLine = newAnswer.answerLine(word);
+        Console.WriteLine(displayLine);
+        Console.WriteLine();
+
+        for (int i = 0; i < 4; i++)
+        {
+            string userGuess = newGuess.getGuess();
+
+            if (parsedWord.Contains(userGuess))
+            {
+                int letterIndex = newWord.getIndex(parsedWord, userGuess);
+                displayLine = newAnswer.newLine(displayLine, userGuess, letterIndex);
+                Console.WriteLine(displayLine);
+                Console.WriteLine();
+                
+                if (displayLine == word) 
+                {
+                    Console.WriteLine("Congratulations! The jumper lived! Thanks for playing.");
+                    Console.WriteLine();
+                    break;
+                }
+
+                i--;
+            }
+            else if (!parsedWord.Contains(userGuess))
+            {
+                Console.WriteLine(displayLine);
+                Console.WriteLine();
+                newGuess.numWrong += 1;
+            }
+
+            if (newGuess.numWrong == 0) 
+            {
+                Console.WriteLine($"{jumper.first}");
+                Console.WriteLine($"{jumper.second}");
+                Console.WriteLine($"{jumper.third}");   
+                Console.WriteLine($"{jumper.fourth}"); 
+                jumper.displayPerson(); 
+                Console.WriteLine();
+            }
+
+            if (newGuess.numWrong == 1) 
+            {
+                jumper.oneWrong();
+                jumper.displayPerson();
+                Console.WriteLine();
+            }
+            
+            if (newGuess.numWrong == 2) 
+            {
+                jumper.twoWrong();
+                jumper.displayPerson();
+                Console.WriteLine();
+            }
+
+            if (newGuess.numWrong == 3) 
+            {
+                jumper.threeWrong();
+                jumper.displayPerson();
+                Console.WriteLine();
+            }
+
+            if (newGuess.numWrong == 4) 
+            {
+                Console.WriteLine();
+                jumper.fourWrong();
+                Console.WriteLine();
+                Console.WriteLine("Looks like our jumper died... You are a terrible person. So long!");
+                break;
+            }
+        }
+
+        // Console.WriteLine(jumper.first);
+    }
+}
